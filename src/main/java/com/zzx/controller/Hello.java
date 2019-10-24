2ba36ebdfa9b73cf59bf0ead2c57f41d84ab7096
kevin <2690966127@qq.com>
2019-10-24 23:34:53 +0800

    aaa

--- src/main/java/com/zzx/controller/Hello.java
+++ src/main/java/com/zzx/controller/Hello.java
@@ -1,18 +1,26 @@
 package com.zzx.controller;
 
+import com.zzx.model.User;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * kevin
+ *
+ * @RestController 返回体为json格式
+ *
  * Created by Administrator on 2019/10/22.
  */
 @RestController
 public class Hello {
 
-    @RequestMapping("/hell0")
-    public void hello(){
+    @RequestMapping("/hello")
+    public User hello(){
         System.out.print("hello world");
+        User user = new User();
+        user.setName("kevin");
+        user.setPwd("123");
+        return user;
     }
 
 }
---
+++ src/main/java/com/zzx/model/User.java
@@ -0,0 +1,25 @@
+package com.zzx.model;
+
+/**
+ * Created by Administrator on 2019/10/24.
+ */
+public class User {
+    private String name;
+    private String pwd;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPwd() {
+        return pwd;
+    }
+
+    public void setPwd(String pwd) {
+        this.pwd = pwd;
+    }
+}
